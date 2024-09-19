7cb5c57c3b5ac8d3b386059c3d43a21c8175dca8
atilaF03 <144128328+atilaF03@users.noreply.github.com>
2024-09-19 22:08:14 +0000

    atualizar

--- se.py
+++ se.py
@@ -1,9 +1,11 @@
 cond1 = False
-cond2 = True
-cond3 = True
-
+cond2 = False
+cond3 = False
+print(type(cond1))
 
 if cond1:
     print("cod as cond1")
 elif cond2:
     print("cod as cond2 ")
+else : 
+    print("nenhuma condição foi feita")
